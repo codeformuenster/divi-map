46c161177f316eaba15c283e8a534a15ffeaa62e
Thorben Jensen <jensen.thorben@gmail.com>
2020-04-08 22:36:04 +0200

    rename download script

--- scripts/01_download.py
+++ scripts/01_download.py
@@ -5,17 +5,19 @@ import pandas as pd
 from pathlib import Path
 
 # download JSON from API
-r = requests.get('https://www.intensivregister.de/api/public/intensivregister?page=0&size=2000')
+r = requests.get(
+    "https://www.intensivregister.de/api/public/intensivregister?page=0&size=2000"
+)
 
 # get data from JSON
 result: dict = r.json()
-data = result['data']
+data = result["data"]
 
 # parse to pandas
 df = pd.json_normalize(data)
 
 # test results
-assert result['rowCount'] == len(result['data']) == len(df), "wrong number of rows"
+assert result["rowCount"] == len(result["data"]) == len(df), "wrong number of rows"
 
 # save to CSV
 df.to_csv(Path("data/intenstivregister.csv"))
