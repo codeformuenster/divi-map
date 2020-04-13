4bfa0e4ee8a47aa1c9e05dfba4d3a912a224577b
Thorben Jensen <jensen.thorben@gmail.com>
2020-04-13 14:59:37 +0200

    fix hovering by further data cleaning

--- scripts/03_map.py
+++ scripts/03_map.py
@@ -6,37 +6,33 @@ import pandas as pd
 import plotly
 import plotly.express as px
 
-
-# HOVERING EXAMPLE
-# us_cities = pd.read_csv(
-#     "https://raw.githubusercontent.com/plotly/datasets/master/us-cities-top-1k.csv"
-# )
-# fig = px.scatter_mapbox(
-#     us_cities,
-#     lat="lat",
-#     lon="lon",
-#     hover_name="City",
-#     hover_data=["City"],
-#     color_discrete_sequence=["green"],
-#     zoom=3,
-# )
-# fig.update_layout(mapbox_style="carto-darkmatter")
-# fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
-# plotly.offline.plot(fig, filename=str(Path("data/map_us.html")))
-
 CSV = Path("data/processed/df_clean.csv")
 
 hospitals = (
     pd.read_csv(str(CSV))
     .dropna(subset=["statusHighCare"])  # drop rows with NA in color column
-    .filter(items=["id", "latitude", "longitude", "statusHighCare"])  # select columns
+    .filter(
+        items=[
+            "id",
+            "bezeichnung",
+            "faelleCovidAktuell",
+            "latitude",
+            "longitude",
+            "statusLowCare",
+            "statusHighCare",
+            "statusECMO",
+        ]
+    )
+    .query("latitude <= 90")  # latitude <= max value of 90 degrees
+    .query("longitude <= 180")  # longitude <= max value of 180 degrees
 )
 
 fig = px.scatter_mapbox(
     hospitals,
     lat="latitude",
     lon="longitude",
-    hover_data=["id"],
+    hover_name="bezeichnung",
+    hover_data=["faelleCovidAktuell", "statusLowCare", "statusHighCare", "statusECMO"],
     color="statusHighCare",
     color_discrete_sequence=["green", "yellow", "red"],
     zoom=5.2,
