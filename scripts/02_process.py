887b8538ecb43f774f9591da9310e7df9b0e0ff0
Thorben Jensen <jensen.thorben@gmail.com>
2020-04-13 14:35:48 +0200

    drop 'meldebereiche' field, add printing

--- scripts/02_process.py
+++ scripts/02_process.py
@@ -11,6 +11,7 @@ from functional import seq
 # index raw jsons
 json_paths: List[Path] = list(Path("data/raw").glob("*.json"))
 # read raw data sets to list
+print("Reading in raw JSON data...")
 data_dicts: List[pd.DataFrame] = []
 for json_path in json_paths:
     # read raw json to dict
@@ -21,16 +22,21 @@ for json_path in json_paths:
     data_dicts.append(data)
 
 # JSONS TO SINGLE DATAFRAME
+print("Combining data to DataFrame...")
 df = pd.DataFrame()
 for data in data_dicts:
     df_new = pd.json_normalize(data)
     df = pd.concat([df, df_new], ignore_index=True)
 
 # CLEAN DATA
+print("Cleaning data...")
 # drop duplicates, remove duplicated id field, sort by hospital ID
 assert all(df.id == df["krankenhausStandort.id"]), "id fields not redundant!"
 df_clean = (
-    df.drop_duplicates().drop(columns=["krankenhausStandort.id"]).sort_values("id")
+    df.drop(columns=["krankenhausStandort.id"])  # redundant ID
+    .drop(columns=["meldebereiche"])  # field value is a list, fails with 'drop_duplicates'
+    .drop_duplicates()
+    .sort_values(["id", "meldezeitpunkt"])
 )
 # shorten column names
 df_clean.columns = seq(df_clean.columns).map(lambda column: column.split(".")[-1])
